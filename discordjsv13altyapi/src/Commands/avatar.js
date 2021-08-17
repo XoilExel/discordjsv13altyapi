4fd5e71a024c484b4e85403f810b4e20a6b11163
Ahmetdev510 <76014530+Ahmetdev510@users.noreply.github.com>
2021-08-17 19:01:34 +0300

    Add files via upload

---
+++ discordjsv13altyapi/src/Commands/avatar.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const Command = require("../Structures/Command.js");
+
+module.exports = new Command({
+	name: "avatar",
+	description: "Avatarını gösterir.",
+	async run(message, args, client) {
+
+		
+		let user = message.mentions.users.first() || message.author;
+
+
+		const embed = new Discord.MessageEmbed()
+		.setTitle("İşte "+user.tag+" Kullanıcısının Avatarı")
+		.setImage(user.displayAvatarURL({dynamic:true}))
+		.setColor("RANDOM")
+
+		message.channel.send({embeds:[embed]})
+		
+	
+	}
+});
